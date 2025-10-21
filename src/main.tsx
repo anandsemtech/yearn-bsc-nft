8a55c461150f1abab15f67c5c982b1c0676c7e27
Anandan Pandurangan <anand.seed@gmail.com>
2025-10-21 09:10:15 +0530

    appkit order changed

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,11 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { wagmiConfig } from "./lib/reown";
 
+
+
+
+
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
